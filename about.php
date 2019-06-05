6b2d8f287328c98d044895f7ff99e69c3607ddec
Carson P <PembleC@oregonstate.edu>
2019-06-05 14:22:00 -0700

    Add About Page
    Simply added the about page to the Nav bar

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+    // Check that a user is logged in within the $_SESSION variable.
+    
+?>
+<html>
+
+    <head>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+
+<body>
+
+<?php include('includes/header.php') ?>
+
+
+
+</body>
+</html>
