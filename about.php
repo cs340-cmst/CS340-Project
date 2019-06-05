302a56177e627bf74edaf17a6100de01ca1e6ed9
Carson P <PembleC@oregonstate.edu>
2019-06-05 16:04:21 -0700

    Make it pretty
    I just put the CSS inside the HTML page

--- about.php
+++ about.php
@@ -2,30 +2,37 @@
 <?php
     session_start();
     // Check that a user is logged in within the $_SESSION variable.
-    
 ?>
+
 <html>
 
     <head>
         <link rel="stylesheet" href="css/styles.css">
     </head>
 
-
 <body>
+
+<style>
+div {
+    background-color: grey;
+    width: 550px;
+    border: 15px solid black;
+    padding: 50px;
+    margin: 20px;
+}
+</style>
 
 <?php include('includes/header.php') ?>
 
 <p>
-<center><b>About the Developers</b></center>
+    <center><b> About the Developers </b></center>
 </p>
 
 <center>
-<textarea rows="8" cols="100">
-
-This website was created by the following four Oregon State students Timothy O’Rourke, Carson Pemble, Manuel Ochoa-Botello, and Sean Spink. The game was created as a project for the undergraduate course CS 340: Databases. This projects tests the ability to design and implement a web-based relational database system, using both php and an open-source database development system such as SQL. This project started with a project proposal and moved towards the creation of an ER diagram, relational schema, website layout and implementation, and finally to a peer review and class presentation.
-
-</textarea>
-</centered>
+    <div>
+        This website was created by the following four Oregon State students Timothy O’Rourke, Carson Pemble, Manuel Ochoa-Botello, and Sean Spink. The game was created as a project for the undergraduate course CS 340: Databases. This projects tests the ability to design and implement a web-based relational database system, using both php and an open-source database development system such as SQL. This project started with a project proposal and moved towards the creation of an ER diagram, relational schema, website layout and implementation, and finally to a peer review and class presentation.
+    </div>
+</center>
 
 </body>
 </html>
