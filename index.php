2e50e64b67228f4452cf7120e8f1664aa2e74aba
orourtim <orourtim@oregonstate.edu>
2019-06-05 23:18:02 -0700

    Setup AJAX to query for Top 3.

--- index.php
+++ index.php
@@ -30,15 +30,76 @@
 
 <div class="column">
     <div id="top-3-boxes">
-    <div class="top-3-box" id="front-page-most-wins">
-        temp
-    </div>
+        <div class="top-3-box" id="front-page-most-kills">
+            <div id="top-kills">Top Kills</div>
+            <div id="most-kills-1"></div>
+            <div id="most-kills-2"></div>
+            <div id="most-kills-3"></div>
+        </div>
 
-    <div class="top-3-box" id="front-page-best-ratio">
-        temp
-    </div>
+        <div class="top-3-box" id="front-page-highest-level">
+            <div id="highest-level">Highest Level</div>
+            <div id="level-1"></div>
+            <div id="level-2"></div>
+            <div id="level-3"></div>
+        </div>
     </div>
 </div>
 
+
+<script>
+    
+    window.onload = function() {
+        getMostKills();
+        getHighestLevel();
+    }
+
+    function getMostKills() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var obj = JSON.parse(this.responseText);
+                fillMostKills(obj);
+            }
+        };
+        xhttp.open("GET", "server/getMostKills.php", true);
+        xhttp.send();
+    }
+
+    function getHighestLevel() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var obj = JSON.parse(this.responseText);
+                fillHighestLevel(obj);
+            }
+        };
+        xhttp.open("GET", "server/getHighestLevel.php", true);
+        xhttp.send();
+    }
+
+    function fillMostKills(obj) {
+        var one = document.getElementById("most-kills-1");
+        var two = document.getElementById("most-kills-2");
+        var three = document.getElementById("most-kills-3");
+
+        one.innerText = "1. " + obj[0].name + " " + obj[0].kills + " kills";
+        two.innerText = "2. " + obj[1].name + " " + obj[1].kills + " kills";
+        three.innerText = "3. " + obj[2].name + " " + obj[2].kills + " kills";
+    }
+
+    function fillHighestLevel(obj) {
+        var one = document.getElementById("level-1");
+        var two = document.getElementById("level-2");
+        var three = document.getElementById("level-3");
+
+        one.innerText = "1. " + obj[0].name + " " + "Level: " + obj[0].kills;
+        two.innerText = "2. " + obj[1].name + " " + "Level: " + obj[1].kills;
+        three.innerText = "3. " + obj[2].name + " " + "Level: " + obj[2].kills;
+    }
+    
+</script>
+
 </body>
+
 </html>
---
+++ server/getMostKills.php
@@ -0,0 +1,5 @@
+<?php
+$array = array(array("name" => "Egg", "kills" => 67), array("name" => "Bread", "kills" => 45), array("name" => "Bob", "kills" => 22));
+
+echo json_encode($array);
+?>
